d58add8cd18970bfb2343b1dd48cac394b7227e0
thomasnrggo <thomasnrggo@gmail.com>
2019-01-22 14:56:40 -0600

    geting access to camera roll

--- src/screens/containers/add-media.js
+++ src/screens/containers/add-media.js
@@ -5,10 +5,14 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { RNCamera } from 'react-native-camera'
 import { Icon, Button } from 'native-base'
+
+// import CameraRollPicker from 'react-native-camera-roll-picker'
+import ImagePicker from 'react-native-image-picker';
 // components
 import CameraLayout from './camera'
 
@@ -26,20 +30,93 @@ export default class AddMedia extends Component {
     )
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageUpdated: '',
+      imagePath: '',
+      imageHeight: '',
+      imageWidth: '',
+    }
+  }
+
   handleCameraClick = () => {
     this.props.navigation.navigate('Camera');
   }
 
+  openImagePiker = () => {
+    const options = {
+      title: 'Seleccionar imagen',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      }
+    }
+
+    ImagePicker.launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        let source = { uri: response.uri};
+
+        this.setState({
+          imageUpdated: source,
+          imagePath: response.uri,
+          imageHeight: response.height,
+          imageWidth: response.width
+        })
+      }
+    })
+  }
+
   render() {
     return (
-      <SafeAreaView>
-        <Text>I'm the add media component</Text>
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={this.state.imageUpdated}
+          style={{width: 500, height: 500,}}
+        />
         <Button
+          dark
+          transparent
+          full
+          bordered
+          style={styles.btn}
           onPress={this.handleCameraClick}
         >
-          <Text>Take Picture</Text>
+          <Text>Tomar foto</Text>
         </Button>
+        <Button
+          dark
+          transparent
+          full
+          bordered
+          style={styles.btn}
+          onPress={this.openImagePiker}
+        >
+          <Text>Desde Galeria</Text>
+        </Button>
+
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  btn: {
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderRadius: 12,
+    backgroundColor: '#dedede',
+    borderColor: '#bbbbbb',
+  }
+})
--- src/screens/containers/camera.js
+++ src/screens/containers/camera.js
@@ -4,12 +4,13 @@ import {
   Text,
   StyleSheet,
   SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 import { RNCamera } from 'react-native-camera'
 import { Icon } from 'native-base'
 
 export default class Camera extends Component {
-  
+
   static navigationOptions = {
     header: null,
   }
@@ -17,14 +18,55 @@ export default class Camera extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text>I'm the Camera component</Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
