009a98d85000b9a800491fbb204b4910a3d64e65
thomasnrggo <thomasnrggo@gmail.com>
2019-01-28 13:43:44 -0600

    moving data to state

--- src/sections/containers/profile-body.js
+++ src/sections/containers/profile-body.js
@@ -15,71 +15,58 @@ let { height, width } = Dimensions.get('window');
 // components
 import Card from '../../cardComponent/containers/card'
 
-var images = [
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-]
-
-const data = [
-  {
-    "id": "10",
-    "username": "TitanLoco8",
-    "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
-    "type": "picture",
-    "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "liked": true,
-    "likes": 100,
-    "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
-    "createAt": "23 Jan 19",
-  },
-  {
-    "id": "11",
-    "username": "TitanLoco8",
-    "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
-    "type": "picture",
-    "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "liked": true,
-    "likes": 100,
-    "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
-    "createAt": "23 Jan 19",
-  },
-  {
-    "id": "12",
-    "username": "TitanLoco8",
-    "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
-    "type": "picture",
-    "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "liked": true,
-    "likes": 100,
-    "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
-    "createAt": "23 Jan 19",
-  },
-  {
-    "id": "13",
-    "username": "TitanLoco8",
-    "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
-    "type": "picture",
-    "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
-    "liked": true,
-    "likes": 100,
-    "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
-    "createAt": "23 Jan 19",
-  },
-]
-
 class ProfileNav extends Component {
   constructor(props) {
     super(props)
     this.state = {
       activeIndex: 0,
+      data:
+      [
+        {
+          "id": "10",
+          "username": "TitanLoco8 ",
+          "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
+          "type": "picture",
+          "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
+          "liked": true,
+          "likes": 100,
+          "description": " Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
+          "createAt": "23 Jan 19",
+        },
+        {
+          "id": "11",
+          "username": "TitanLoco8",
+          "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
+          "type": "picture",
+          "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
+          "liked": true,
+          "likes": 100,
+          "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
+          "createAt": "23 Jan 19",
+        },
+        {
+          "id": "12",
+          "username": "TitanLoco8",
+          "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
+          "type": "picture",
+          "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
+          "liked": true,
+          "likes": 100,
+          "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
+          "createAt": "23 Jan 19",
+        },
+        {
+          "id": "13",
+          "username": "TitanLoco8",
+          "profileImage": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/profile.jpg?alt=media&token=33d8dbd0-3424-4753-9ec5-1c6a21979ab7",
+          "type": "picture",
+          "picture": "https://firebasestorage.googleapis.com/v0/b/magistant-c3788.appspot.com/o/example-01.jpg?alt=media&token=9512ab93-dda2-437e-bf65-bcb27f7fd0a1",
+          "liked": true,
+          "likes": 100,
+          "description": "Con el tiempo, sabrán lo que es perder, sentir en lo mas profundo que tienes razon y aun asi, fallar.",
+          "createAt": "23 Jan 19",
+        },
+      ],
     }
   }
 
@@ -96,7 +83,8 @@ class ProfileNav extends Component {
   }
 
   renderGridSection = () => {
-    let imagesForGrid = data.map((data) => data.picture)
+    const data = this.state.data
+    const imagesForGrid = data.map((data) => data.picture)
     return imagesForGrid.map((picture, index) => {
       return (
           <View>
@@ -139,7 +127,7 @@ class ProfileNav extends Component {
       return (
             <FlatList
               style={{flex:1}}
-              data={data}
+              data={this.state.data}
               keyExtractor={this._keyExtractor}
               renderItem={this.renderListSection}
             />
